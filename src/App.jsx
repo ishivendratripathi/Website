daa0e3f48aecb87092acf5c5524e349460e6350e
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-07-11 22:34:10 +0530

    this is the website repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>HEY WELCOME TO THIS PROJECT ,lets ROCK THIS </h1>
+    </>
+  )
+}
+
+export default App
