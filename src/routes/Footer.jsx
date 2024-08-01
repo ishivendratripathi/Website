5d5b0bb20dd065ef3c219fea938b4996afadb8cf
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-08-01 23:21:11 +0530

    new updates

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className="w-full h-96 bg-fuchsia-200 flex relative">
+    <section className="w-full h-96 bg-fuchsia-200 flex">
   <img className="h-16 m-20"src="logo.png" alt="" />
-  <h2 className='mt-40 z-10 font-bold text-3xl text-green-700'>contact us</h2>
-  <h3 className=''>123 Anywhere St. Any City, ST 12345 Country</h3>
+  <h2 className='mt-40 ml-96 z-10 font-bold text-3xl text-green-700 absolute'>contact us</h2>
+  <img className="h-60 mt-32 ml-20" src="contact.png" alt="" />
+  
     </section>
   )
 }
