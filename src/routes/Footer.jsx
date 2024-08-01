cb16b761a292654044039632ca1735e86c4117e0
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-08-02 00:44:11 +0530

    updates

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className="w-full h-96 bg-fuchsia-200 flex">
-  <img className="h-16 m-20"src="logo.png" alt="" />
-  <h2 className='mt-40 ml-96 z-10 font-bold text-3xl text-green-700 absolute'>contact us</h2>
-  <img className="h-60 mt-32 ml-20" src="contact.png" alt="" />
-  
-    </section>
+    <section className="w-full h-lvh bg-fuchsia-200 flex">
+      <img className="h-16 m-20" src="logo.png" alt="" />
+      <h2 className='mt-96 ml-96 font-bold text-3xl text-green-700 absolute'>contact us
+      <h2 className='font-normal text-sm mt-28'>123 Anywhere <br /> St.Any City, ST 12345 <br /> Country</h2>
+       </h2>
+       <h2 className='z-5'>bkbkkbk</h2>
+      <img className="h-80 ml-20 mt-80 z--1 absolute" src="contact.png" alt="" />
+        </section>
   )
 }
 export default Footer
