95325e901147d895722491d7465a59dd21d3ae4c
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-08-02 22:35:43 +0530

    new updates

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <section className="w-full h-lvh bg-fuchsia-200 flex">
       <img className="h-16 m-20" src="logo.png" alt="" />
-      <h2 className='mt-96 ml-96 font-bold text-3xl text-green-700 absolute'>contact us
+      <h2 className='mt-96 ml-96 font-bold text-3xl text-green-700'>contact us
       <h2 className='font-normal text-sm mt-28'>123 Anywhere <br /> St.Any City, ST 12345 <br /> Country</h2>
        </h2>
        <h2 className='z-5'>bkbkkbk</h2>
-      <img className="h-80 ml-20 mt-80 z--1 absolute" src="contact.png" alt="" />
+      <img className="h-80 ml-20 mt-80 z--1" src="contact.png" alt="" />
         </section>
   )
 }
