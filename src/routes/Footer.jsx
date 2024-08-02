881a1ceb1228e55dabe1b03d0be24578aa6a97ba
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-08-03 01:33:48 +0530

    these are the new changes

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -3,12 +3,30 @@ import React from 'react'
 const Footer = () => {
   return (
     <section className="w-full h-lvh bg-fuchsia-200 flex">
-      <img className="h-16 m-20" src="logo.png" alt="" />
-      <h2 className='mt-96 ml-96 font-bold text-3xl text-green-700'>contact us
-      <h2 className='font-normal text-sm mt-28'>123 Anywhere <br /> St.Any City, ST 12345 <br /> Country</h2>
-       </h2>
-       <h2 className='z-5'>bkbkkbk</h2>
-      <img className="h-80 ml-20 mt-80 z--1" src="contact.png" alt="" />
+<img  className="h-12 m-20" src="logo.png" alt="" />
+ <img className="h-72 mt-80 z-1 absolute ml-20 w-2/3" src="contact.png" alt="" />
+ <h2 className='z-20 mt-96 font-bold text-green-600 text-2xl'>Contact us
+  <h2 className='text-sm mt-20 text-black flex'>123 Anywhere St. <br />Any City,ST <br />12345 Country</h2>
+ </h2>
+ <h2 className='mt-96 pl-60 pt-20 z-50 font-bold text-black'>Telephone-9786675867586
+ <h2 className='text-black font-bold'>Mobile-98765422467</h2>
+ <br />
+ <h2 className=''>hello@greatsite.com</h2>
+ </h2>
+
+ <h1 className='text-green-800 font-extrabold text-2xl mt-96 ml-80'>Office Hours
+  
+  <br />
+  <br />
+  <h2 className='text-base text-black font-light'>Monday: 8am - 7pm</h2>
+  <h2 className='text-base text-black font-light'>Tuesday: 8am - 7pm</h2>
+  <h2 className='text-base text-black font-light'>Wednesday: 8am - 7pm</h2>
+  <h2 className='text-base text-black font-light'>Thursday: 8am - 7pm</h2>
+  <h2 className='text-base text-black font-light'>Friday: 8am - 7pm</h2>
+
+
+ </h1>
+
         </section>
   )
 }
