274638891932641f1e87c3b920c46e5d65ec5968
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-08-12 18:33:30 +0530

    about us

--- src/routes/Aboutus.jsx
+++ src/routes/Aboutus.jsx
@@ -3,12 +3,12 @@ import Faq from './Faq'
 
 export default function Aboutus() {
   return (
-    <div className='h-lvh bg-#526c3c'>
-     <h2 className='relative top-20 left-40 text-3xl font-bold text-fuchsia-200'>
-        About us
-     </h2>
-    
-    
+    <div className='h-lvh bg-green-900'>
+
+<div className='grid grid-cols-2'>
+  <h2 className='cols-cols-1 flex relative top-10'>About us </h2>
+</div>
+
     </div>
-  )
+    )
 }
