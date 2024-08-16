351e94c18fd9fba09e6657cd7e4117e75e76cf8e
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2024-08-16 23:51:29 +0530

    updates

--- src/routes/Aboutus.jsx
+++ src/routes/Aboutus.jsx
@@ -6,7 +6,22 @@ export default function Aboutus() {
     <div className='h-lvh bg-green-900'>
 
 <div className='grid grid-cols-2'>
-  <h2 className='col-span-2 flex relative top-10 left-20'>About us </h2>
+  <h2 className='text-3xl font-bold mt-28 ml-36 h-80 w-6/12'>
+    <div className='text-fuchsia-300'>About us 
+    </div>
+    <div className='text-sm mt-10'> 
+      <div className='text-white'>
+    In our fast-paced world, maintaining mental well-
+being can be challenging. This article explores five
+simple and effective practices that you can
+incorporate into your daily routine to enhance your
+mental health. From mindfulness exercises to
+physical activity, discover practical tips to boost
+your mood and overall well-being
+</div>
+</div>
+  </h2>
+  <h2 className='bg-blue-700 mt-20'>WHAT WE DO</h2>  
 </div>
 
     </div>
